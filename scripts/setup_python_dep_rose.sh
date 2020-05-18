ea3f246459fdda5c91888bb7833f423c67e659f2
Giulio Sidoretti <giuliosidoretti@gmail.com>
2020-05-18 17:56:45 +0200

    add grpc in rose venv

--- scripts/setup_python_dep_rose.sh
+++ scripts/setup_python_dep_rose.sh
@@ -10,6 +10,16 @@ echo -e "\n\n#####################################"
 echo -e "\n-Installing wheel"
 pip3 install wheel &&
 
+# Install grpcio
+echo -e "\n\n#####################################"
+echo -e "\n-Installing grpcio"
+pip3 install grpcio &&
+
+# Install grpcio-tools
+echo -e "\n\n#####################################"
+echo -e "\n-Installing grpcio-tools"
+pip3 install grpcio-tools &&
+
 # Install python-arango
 echo -e "\n\n#####################################"
 echo -e "\n-Installing python-arango"
