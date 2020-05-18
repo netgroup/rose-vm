f7767876d7d890d8ffbaebcfba08d66a0b3f14fd
Giulio Sidoretti <giuliosidoretti@gmail.com>
2020-05-18 18:42:36 +0200

    added modules python-hosts and python-dotenv

--- scripts/setup_python_dep_mininet.sh
+++ scripts/setup_python_dep_mininet.sh
@@ -14,3 +14,13 @@ pip3 install wheel &&
 echo -e "\n\n#####################################"
 echo -e "\n-Installing mininet"
 pip3 install git+https://github.com/mininet/mininet.git
+
+# Install python-hosts
+echo -e "\n\n#####################################"
+echo -e "\n-Installing python-hosts"
+pip3 install python-hosts
+
+# Install python-dotenv
+echo -e "\n\n#####################################"
+echo -e "\n-Installing python-dotenv"
+pip3 install python-dotenv
